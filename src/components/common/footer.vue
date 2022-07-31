793338453489614a416502aa2a172cc849f4222a
Shadowkl <1451228146@qq.com>
2022-07-31 09:12:39 +0800

    Vue

---
+++ src/components/common/footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class='footer'>
+        <div class="w1200">
+            <p>影子的寄居地</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {},
+    data () {
+        // 这里存放数据
+        return {
+        }
+    },
+    // 监听属性 类似于data概念
+    computed: {},
+    // 方法集合
+    methods: {
+    }
+}
+</script>
+<style scoped lang="less">
+.footer {
+    line-height: 40px;
+    background: #06060b;
+    color: #60606d;
+}
+</style>
